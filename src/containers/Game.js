9a5033f37ce6b6b3b24a0209137562bc25fb9ebb
Abraham Lopez <abethebest111@gmail.com>
2026-05-01 23:27:54 -0700

    Create containers directory and added Game.js

---
+++ src/containers/Game.js
@@ -0,0 +1,16 @@
+// src/containers/Game.js
+import React, {Component} from 'react';
+import App from '../App';
+import carsGame from '../utils/carsGame.js';
+
+let theCars = new carsGame();
+
+class Game extends Component
+{
+    render()
+    {
+        return ( <App cars={theCars} />);
+    }
+};
+
+export default Game;
